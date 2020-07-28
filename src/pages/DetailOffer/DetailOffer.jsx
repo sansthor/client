a7e9493bf8ba45e8e2f220a440328ffbfee2f8b4
agusTrih <ant3dong@gmail.com>
2020-07-29 00:47:08 +0700

    fix need review

--- src/pages/DetailOffer/DetailOffer.jsx
+++ src/pages/DetailOffer/DetailOffer.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Header from "../../components/Header/Header";
 import Styled from "styled-components";
 import { Form, CustomInput, ButtonToggle } from "reactstrap";
+import Footer from "../../components/Footer/Footer";
 // --- Styled Components ---
 const Container = Styled.div`
 padding: 100px;
@@ -49,82 +50,90 @@ margin-left: 2rem;
 @media (max-width: 960px) {
     text-align: center;
     }
+@media (max-width: 600px) {
+    
+    margin-left: 1px;
+    }
 `;
 // --- Styled Components ---
 
 function DetailOffer() {
     return (
-        <Container>
-            <Tittle>Tittle Product</Tittle>
-            <Wraps>
-                <ContentWrapLeft>
-                    <ContentWrapDetail>
-                        <p>@username</p>
-                        <span style={{ margin: "0 5px" }}>||</span>
-                        <span>Icon Star</span>
-                    </ContentWrapDetail>
+        <div>
+            <Header />
+            <Container>
+                <Tittle>Tittle Product</Tittle>
+                <Wraps>
+                    <ContentWrapLeft>
+                        <ContentWrapDetail>
+                            <p>@username</p>
+                            <span style={{ margin: "0 5px" }}>||</span>
+                            <span>Icon Star</span>
+                        </ContentWrapDetail>
 
-                    <ImageWrap>
-                        <div style={{ width: "50%" }}>
-                            <img
-                                src="https://images.unsplash.com/photo-1487412720507-e7ab37603c6f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=751&q=80"
-                                alt="images-product"
-                                style={{ width: "100%" }}
-                            />
-                        </div>
-                        <div>
-                            <h2>Description</h2>
-                            <p>
-                                Lorem ipsum dolor sit amet consectetur,
-                                adipisicing elit. Id asperiores facere nam
-                                doloribus dolorem, maiores rerum consectetur
-                                consequuntur. Voluptates distinctio sequi nihil
-                                explicabo impedit. Reprehenderit fugiat fuga
-                                odio animi sit.
-                            </p>
-                        </div>
-                    </ImageWrap>
-                </ContentWrapLeft>
-                <ContentWrapRight>
-                    <ContentWrapDetail>
-                        <p>Offer Feature</p>
-                    </ContentWrapDetail>
+                        <ImageWrap>
+                            <div style={{ width: "50%" }}>
+                                <img
+                                    src="https://images.unsplash.com/photo-1487412720507-e7ab37603c6f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=751&q=80"
+                                    alt="images-product"
+                                    style={{ width: "100%" }}
+                                />
+                            </div>
+                            <div style={{ padding: "1rem" }}>
+                                <h2>Description</h2>
+                                <p>
+                                    Lorem ipsum dolor sit amet consectetur,
+                                    adipisicing elit. Id asperiores facere nam
+                                    doloribus dolorem, maiores rerum consectetur
+                                    consequuntur. Voluptates distinctio sequi
+                                    nihil explicabo impedit. Reprehenderit
+                                    fugiat fuga odio animi sit.
+                                </p>
+                            </div>
+                        </ImageWrap>
+                    </ContentWrapLeft>
+                    <ContentWrapRight>
+                        <ContentWrapDetail>
+                            <p>Offer Feature</p>
+                        </ContentWrapDetail>
 
-                    <ContentWrapDetail>
-                        <Form
-                            style={{
-                                display: "flex",
-                                flexDirection: "column",
-                                border: "1px solid gray",
-                                padding: "25px",
-                            }}
-                        >
-                            <CustomInput
-                                id="exampleCustomCheckbox"
-                                type="checkbox"
-                                label="Revision 2 times"
-                            />
-                            <CustomInput
-                                id="exampleCustomCheckbox2"
-                                type="checkbox"
-                                label="Commercial use"
-                            />
-                            <CustomInput
-                                id="exampleCustomCheckbox3"
-                                type="checkbox"
-                                label="Commercial use"
-                            />
-                            <ButtonToggle
-                                color="primary"
-                                style={{ marginTop: "30px" }}
+                        <ContentWrapDetail>
+                            <Form
+                                style={{
+                                    display: "flex",
+                                    flexDirection: "column",
+                                    border: "1px solid gray",
+                                    padding: "25px",
+                                }}
                             >
-                                primary
-                            </ButtonToggle>
-                        </Form>
-                    </ContentWrapDetail>
-                </ContentWrapRight>
-            </Wraps>
-        </Container>
+                                <CustomInput
+                                    id="exampleCustomCheckbox"
+                                    type="checkbox"
+                                    label="Revision 2 times"
+                                />
+                                <CustomInput
+                                    id="exampleCustomCheckbox2"
+                                    type="checkbox"
+                                    label="Commercial use"
+                                />
+                                <CustomInput
+                                    id="exampleCustomCheckbox3"
+                                    type="checkbox"
+                                    label="Commercial use"
+                                />
+                                <ButtonToggle
+                                    color="primary"
+                                    style={{ marginTop: "30px" }}
+                                >
+                                    primary
+                                </ButtonToggle>
+                            </Form>
+                        </ContentWrapDetail>
+                    </ContentWrapRight>
+                </Wraps>
+            </Container>
+            <Footer />
+        </div>
     );
 }
 
