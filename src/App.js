a3eeda79eada896b589225b2e8373049ec9cb508
renvelvet <reshapuspita734@gmail.com>
2020-07-28 11:47:56 +0700

    add files

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
+import styled from "styled-components";
+// import logo from './logo.svg';
+// import './App.css';
+
+import reducers from "./redux/reducers";
+import PrivateRoute from "./helpers/PrivateRoute";
+
+function App() {
+  return (
+    <Provider store={createStore(reducers, applyMiddleware(thunk))}>
+      <Router>
+        <Switch>
+          {/* <Route exact path="/">
+                        <Home />
+                    </Route>
+                    <Route exact path="/register">
+                        <Register />
+                    </Route>
+                    <Route exact path="/login">
+                        <Login />
+                    </Route>
+                    <PrivateRoute exact path="/userPage">
+                        <ListRestaurant />
+                    </PrivateRoute>
+                    <PrivateRoute exact path="/restaurants/:id">
+                        <DescriptionRestaurants />
+                    </PrivateRoute> */}
+        </Switch>
+      </Router>
+    </Provider>
+  );
+}
+
+export default App;
