6291f65e9062a9bbcb599c24c5a67ba6b3bee526
agusTrih <ant3dong@gmail.com>
2020-07-29 12:52:28 +0700

    sketsa done

--- src/pages/PaymentMethod/PaymentMethod.jsx
+++ src/pages/PaymentMethod/PaymentMethod.jsx
@@ -12,6 +12,16 @@ display: flex;
 flex-direction: column;
 width: 60%;
 `;
+const WrapRight = Styled.div`
+display: flex;
+flex-direction: column;
+width: 20%;
+`;
+const Wraps = Styled.div`
+display: flex;
+flex-direction: row;
+justify-content: space-around;
+`;
 // --- Styled Components ---
 
 function PaymentMethod() {
@@ -54,6 +64,66 @@ function PaymentMethod() {
                 bgcolor1: "none",
                 bgcolor2: "none",
                 bgcolor3: "primary",
+                bgcolor4: "none",
+                bgcolor5: "none",
+                bgcolor6: "none",
+                bgcolor7: "none",
+                bgcolor8: "none",
+            });
+        } else if (event.target.name === "bgColor4") {
+            setBgColor({
+                bgcolor1: "none",
+                bgcolor2: "none",
+                bgcolor3: "none",
+                bgcolor4: "primary",
+                bgcolor5: "none",
+                bgcolor6: "none",
+                bgcolor7: "none",
+                bgcolor8: "none",
+            });
+        } else if (event.target.name === "bgColor5") {
+            setBgColor({
+                bgcolor1: "none",
+                bgcolor2: "none",
+                bgcolor3: "none",
+                bgcolor4: "none",
+                bgcolor5: "primary",
+                bgcolor6: "none",
+                bgcolor7: "none",
+                bgcolor8: "none",
+            });
+        } else if (event.target.name === "bgColor6") {
+            setBgColor({
+                bgcolor1: "none",
+                bgcolor2: "none",
+                bgcolor3: "none",
+                bgcolor4: "none",
+                bgcolor5: "none",
+                bgcolor6: "primary",
+                bgcolor7: "none",
+                bgcolor8: "none",
+            });
+        } else if (event.target.name === "bgColor7") {
+            setBgColor({
+                bgcolor1: "none",
+                bgcolor2: "none",
+                bgcolor3: "none",
+                bgcolor4: "none",
+                bgcolor5: "none",
+                bgcolor6: "none",
+                bgcolor7: "primary",
+                bgcolor8: "none",
+            });
+        } else if (event.target.name === "bgColor8") {
+            setBgColor({
+                bgcolor1: "none",
+                bgcolor2: "none",
+                bgcolor3: "none",
+                bgcolor4: "none",
+                bgcolor5: "none",
+                bgcolor6: "none",
+                bgcolor7: "none",
+                bgcolor8: "primary",
             });
         }
     };
@@ -64,84 +134,91 @@ function PaymentMethod() {
                     <h1 className="display-3">Select Payment Method</h1>
                 </Container>
             </Jumbotron>
-            <WrapLeft>
-                <div>
-                    <ButtonToggle
-                        name="bgColor1"
-                        color={bgColor.bgcolor1}
-                        onClick={handleChange}
-                    >
-                        CreditCard
-                    </ButtonToggle>
-                    <ButtonToggle
-                        name="bgColor2"
-                        color={bgColor.bgcolor2}
-                        onClick={handleChange}
-                    >
-                        Paypal
-                    </ButtonToggle>
-                    <ButtonToggle
-                        name="bgColor3"
-                        color={bgColor.bgcolor3}
-                        onClick={handleChange}
-                    >
-                        Amazon Web Service
-                    </ButtonToggle>
-                </div>
-                {/*  */}
-                <FlexColumn>
-                    <input type="text" />
-                    <input type="number" />
-                    <div
-                        style={{
-                            display: "flex",
-                            justifyContent: "space-between",
-                        }}
-                    >
-                        <input />
-                        <input />
-                        <input />
+            <Wraps>
+                <WrapLeft>
+                    <div>
+                        <ButtonToggle
+                            name="bgColor1"
+                            color={bgColor.bgcolor1}
+                            onClick={handleChange}
+                        >
+                            CreditCard
+                        </ButtonToggle>
+                        <ButtonToggle
+                            name="bgColor2"
+                            color={bgColor.bgcolor2}
+                            onClick={handleChange}
+                        >
+                            Paypal
+                        </ButtonToggle>
+                        <ButtonToggle
+                            name="bgColor3"
+                            color={bgColor.bgcolor3}
+                            onClick={handleChange}
+                        >
+                            Amazon Web Service
+                        </ButtonToggle>
                     </div>
-                </FlexColumn>
-            </WrapLeft>
-            <div>
-                {" "}
-                <ButtonToggle
-                    name="bgColor4"
-                    color={bgColor.bgcolor4}
-                    onClick={handleChange}
-                >
-                    CreditCard
-                </ButtonToggle>
-                <ButtonToggle
-                    name="bgColor5"
-                    color={bgColor.bgcolor5}
-                    onClick={handleChange}
-                >
-                    Paypal
-                </ButtonToggle>
-                <ButtonToggle
-                    name="bgColor6"
-                    color={bgColor.bgcolor6}
-                    onClick={handleChange}
-                >
-                    Amazon Web Service
-                </ButtonToggle>
-                <ButtonToggle
-                    name="bgColor7"
-                    color={bgColor.bgcolor7}
-                    onClick={handleChange}
-                >
-                    Amazon Web Service
-                </ButtonToggle>
-                <ButtonToggle
-                    name="bgColor8"
-                    color={bgColor.bgcolor8}
-                    onClick={handleChange}
-                >
-                    Amazon Web Service
-                </ButtonToggle>
-            </div>
+                    {/*  */}
+                    <FlexColumn>
+                        <input type="text" />
+                        <input type="number" />
+                        <div
+                            style={{
+                                display: "flex",
+                                justifyContent: "space-between",
+                            }}
+                        >
+                            <input />
+                            <input />
+                            <input />
+                        </div>
+                    </FlexColumn>
+                </WrapLeft>
+                <WrapRight>
+                    {" "}
+                    <ButtonToggle
+                        style={{ border: "1px solid black" }}
+                        name="bgColor4"
+                        color={bgColor.bgcolor4}
+                        onClick={handleChange}
+                    >
+                        ATM
+                    </ButtonToggle>
+                    <ButtonToggle
+                        style={{ border: "1px solid black" }}
+                        name="bgColor5"
+                        color={bgColor.bgcolor5}
+                        onClick={handleChange}
+                    >
+                        OVO
+                    </ButtonToggle>
+                    <ButtonToggle
+                        style={{ border: "1px solid black" }}
+                        name="bgColor6"
+                        color={bgColor.bgcolor6}
+                        onClick={handleChange}
+                    >
+                        Go-Pay
+                    </ButtonToggle>
+                    <ButtonToggle
+                        style={{ border: "1px solid black" }}
+                        name="bgColor7"
+                        color={bgColor.bgcolor7}
+                        onClick={handleChange}
+                    >
+                        LinkAja
+                    </ButtonToggle>
+                    <ButtonToggle
+                        style={{ border: "1px solid black" }}
+                        name="bgColor8"
+                        color={bgColor.bgcolor8}
+                        onClick={handleChange}
+                    >
+                        Dana
+                    </ButtonToggle>
+                </WrapRight>
+            </Wraps>
         </div>
     );
 }
