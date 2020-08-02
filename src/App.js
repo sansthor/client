439987f0da3501eacf8af1c7a9f5c686d9369189
agusTrih <ant3dong@gmail.com>
2020-08-02 14:53:58 +0700

    add responsive jumbotron landing-page

--- src/App.js
+++ src/App.js
@@ -1,47 +1,47 @@
-import React from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import { Provider } from "react-redux";
-import { createStore, applyMiddleware } from "redux";
-import thunk from "redux-thunk";
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
 // import styled from "styled-components";
 // import logo from './logo.svg';
 // import './App.css';
 // import Login from './pages/Login/Login'
 
-import reducers from "./redux/reducers";
-import PrivateRoute from "./helpers/PrivateRoute";
+import reducers from './redux/reducers';
+import PrivateRoute from './helpers/PrivateRoute';
 
-import "bootstrap/dist/css/bootstrap.min.css";
-import Header from "./components/Header/Header";
-import LandingPages from "./pages/LandinPages/LandingPages";
-import Login from "./pages/Login/Login";
-import RegisUser from "./pages/RegisUser/RegisUser";
-import Footer from "./components/Footer/Footer";
-import CatalogService from "./pages/CatalogService/CatalogService";
-import DetailOffer from "./pages/DetailOffer/DetailOffer";
-import PaymentMethod from "./pages/PaymentMethod/PaymentMethod";
-import PurchaseHistory from "./components/PurchaseHistory/PurchaseHistory";
-import Order from "./components/Order/Order";
-import MemberFeed from "./pages/MemberFeed/MemberFeed";
-import TalentFeed from "./pages/TalentFeed/TalentFeed";
-import PaymentModal from "./pages/PaymentModal/PaymentModal";
-import Jumbotron from "./components/Jumbortron/Jumbortron";
-import FindDigitalService from "./components/FindDigitalService/FindDigitalService";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Header from './components/Header/Header';
+import LandingPages from './pages/LandinPages/LandingPages';
+import Login from './pages/Login/Login';
+import RegisUser from './pages/RegisUser/RegisUser';
+import Footer from './components/Footer/Footer';
+import CatalogService from './pages/CatalogService/CatalogService';
+import DetailOffer from './pages/DetailOffer/DetailOffer';
+import PaymentMethod from './pages/PaymentMethod/PaymentMethod';
+import PurchaseHistory from './components/PurchaseHistory/PurchaseHistory';
+import Order from './components/Order/Order';
+import MemberFeed from './pages/MemberFeed/MemberFeed';
+import TalentFeed from './pages/TalentFeed/TalentFeed';
+import PaymentModal from './pages/PaymentModal/PaymentModal';
+import Jumbotron from './components/Jumbortron/Jumbortron';
+import FindDigitalService from './components/FindDigitalService/FindDigitalService';
 function App() {
-  return (
-    <Provider store={createStore(reducers, applyMiddleware(thunk))}>
-      <Router>
-        <Switch>
-          <Route exact path="/">
-            <LandingPages />
-          </Route>
-          <Route exact path="/login">
-            <Login />
-          </Route>
-        </Switch>
-      </Router>
-    </Provider>
-  );
+    return (
+        <Provider store={createStore(reducers, applyMiddleware(thunk))}>
+            <Router>
+                <Switch>
+                    <Route exact path="/">
+                        <LandingPages />
+                    </Route>
+                    <Route exact path="/login">
+                        <Login />
+                    </Route>
+                </Switch>
+            </Router>
+        </Provider>
+    );
 }
 
 export default App;
--- src/pages/LandinPages/LandingPages.jsx
+++ src/pages/LandinPages/LandingPages.jsx
@@ -1,24 +1,46 @@
-import React from "react";
+import React from 'react';
+import Styled from 'styled-components';
 
-import { Jumbotron, Container, Button } from "reactstrap";
-import Header from "../../components/Header/Header";
-import Footer from "../../components/Footer/Footer";
-import FindDigitalService from "../../components/FindDigitalService/FindDigitalService";
+import { Jumbotron, Container, Button } from 'reactstrap';
+import Header from '../../components/Header/Header';
+import Footer from '../../components/Footer/Footer';
+import FindDigitalService from '../../components/FindDigitalService/FindDigitalService';
 
+// --- styledComponents --- //
+const Tittle = Styled.h1`
+font-size: 46px;
+width: 50%;
+@media (max-width: 768px) {
+    font-size: 24px;
+    text-align: center;
+    width: 100%;
+  }
+`;
+const Text = Styled.p`
+font-size: 24px;
+@media (max-width: 768px) {
+    font-size: 16px;
+    text-align: center;
+}
+`;
+// --- styledComponents --- //
 function LandingPages() {
     return (
         <div>
             <Header />
-            <Jumbotron fluid>
+            <Jumbotron fluid style={{ padding: '100px 50px' }}>
                 <Container fluid>
-                    <h1 style={{ width: "50%" }} className="display-3">
-                        Help you find the best digital talent that match your
-                        needs!
-                    </h1>
-                    <p className="lead">
+                    <Tittle className="display-3">
+                        <strong>
+                            {' '}
+                            Help you find the best digital talent that match
+                            your needs!
+                        </strong>
+                    </Tittle>
+                    <Text className="lead">
                         Temukan talent terbaik untuk membantu memecahkan
                         masalahmu
-                    </p>
+                    </Text>
                 </Container>
             </Jumbotron>
             <FindDigitalService />
