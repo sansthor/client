96023c749b63d56bd7f1f405cf581cb34fc47d0b
agusTrih <ant3dong@gmail.com>
2020-08-02 18:00:31 +0700

    purchase history my service order responsive

--- src/components/Order/Order.jsx
+++ src/components/Order/Order.jsx
@@ -1,54 +1,52 @@
-import React from "react";
-import { Table, FormGroup, Label, Input, Button } from "reactstrap";
-import Styled from "styled-components";
+import React from 'react';
+import { Table, FormGroup, Label, Input, Button } from 'reactstrap';
+import Styled from 'styled-components';
 
 // --- Styled components ---
-
-const Wraps = Styled.div`
-padding: 50px 50px;
-border: 1px solid black;
-margin: 100px;
+const TableHeading = Styled.th`
+width: "60%";
+@media (max-width: 700px) {
+    width: 50%;
+   }
 `;
 // --- Styled components ---
 function Order() {
     return (
         <div>
-            <Wraps>
-                <Table bordered>
-                    <thead>
-                        <tr>
-                            <th style={{ width: "60%" }}>PESANAN</th>
-                            <th>STATUS</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <td>Design web buat company animekit</td>
-                            <td>
-                                {" "}
-                                <FormGroup
-                                    style={{
-                                        display: "flex",
-                                        flexDirection: "row",
-                                        justifyContent: "space-between",
-                                    }}
+            <Table bordered>
+                <thead>
+                    <tr>
+                        <TableHeading>PESANAN</TableHeading>
+                        <th>STATUS</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>Design web buat company animekit</td>
+                        <td>
+                            {' '}
+                            <FormGroup
+                                style={{
+                                    display: 'flex',
+                                    flexDirection: 'row',
+                                    justifyContent: 'space-between',
+                                }}
+                            >
+                                <Input
+                                    style={{ width: '70%' }}
+                                    type="select"
+                                    name="select"
+                                    id="exampleSelect"
                                 >
-                                    <Input
-                                        style={{ width: "70%" }}
-                                        type="select"
-                                        name="select"
-                                        id="exampleSelect"
-                                    >
-                                        <option>Ongoing</option>
-                                        <option>Done</option>
-                                    </Input>
-                                    <Button color="primary">Confirm</Button>
-                                </FormGroup>
-                            </td>
-                        </tr>
-                    </tbody>
-                </Table>
-            </Wraps>
+                                    <option>Ongoing</option>
+                                    <option>Done</option>
+                                </Input>
+                                <Button color="primary">Confirm</Button>
+                            </FormGroup>
+                        </td>
+                    </tr>
+                </tbody>
+            </Table>
         </div>
     );
 }
