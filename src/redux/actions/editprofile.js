d8f1a3808b4ebf5df7d1b3342304cdd4247f915b
renvelvet <reshapuspita734@gmail.com>
2020-08-06 13:52:34 +0700

    add actions editprofile

---
+++ src/redux/actions/editprofile.js
@@ -0,0 +1,29 @@
+import Swal from 'sweetalert2';
+import jwt_decode from 'jwt-decode';
+
+const editprofile = (formData, history) => async (dispatch) => {
+    const decoded = jwt_decode(localStorage.getItem('token'));
+    const id = decoded.id;
+    const url = `${process.env.REACT_APP_API_URL}/user/update/${id}`;
+    const options = {
+        method: 'PUT',
+        body: JSON.stringify({ ...formData }),
+        headers: {
+            'Content-type': 'application/json',
+        },
+    };
+
+    const response = await fetch(url, options);
+    await response.json();
+
+    if (response.status === 200) {
+        Swal.fire({
+            title: 'Profile Berhasil Diperbarui',
+            text: '',
+            icon: 'success',
+        });
+        history.push('/profile');
+    }
+};
+
+export { editprofile };
