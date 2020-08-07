a35ed2ee4722abb702b7576e438ea9cfaa0f18e9
renvelvet <reshapuspita734@gmail.com>
2020-08-07 19:23:27 +0700

    add card di my services

--- src/components/CardMyServices/CardMyServices.jsx
+++ src/components/CardMyServices/CardMyServices.jsx
@@ -1,33 +1,34 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { fetchGetOrder } from '../../redux/actions';
+import { fetchGetServiceTalent } from '../../redux/actions';
 import CardRiwayatMyServices from '../CardComponent/CardRiwayatMyServices';
 
 function CardMyServices() {
     const dispatch = useDispatch();
-    const order = useSelector((state) => state.getorder);
+    const service = useSelector((state) => state.getservicetalent);
+    console.log(service);
 
     useEffect(() => {
-        dispatch(fetchGetOrder());
+        dispatch(fetchGetServiceTalent());
     }, [dispatch]);
 
     return (
         <React.Fragment>
-            {order !== undefined &&
-                (order.length === 0 ? (
+            {service.data !== undefined &&
+                (service.data.length === 0 ? (
                     <p>Kosong</p>
                 ) : (
-                    order.length > 0 &&
-                    order.map((item) => {
+                    service.data.length > 0 &&
+                    service.data.map((item) => {
                         console.log(item, 'kk');
                         return (
                             <CardRiwayatMyServices
-                                image={item.serviceID.image}
-                                title={item.serviceID.title}
-                                name={item.talentID.username}
+                                image={item.image}
+                                title={item.title}
+                                name={item.userID.username}
                                 avatar={item.userID.avatar}
                                 key={item._id}
-                                price={item.total}
+                                price={item.price}
                             />
                         );
                     })
