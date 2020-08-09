8323288540a91bbadc15839f20cdac12c89e3d02
agusTrih <ant3dong@gmail.com>
2020-08-09 07:47:02 +0700

    edit design bantuan

--- src/components/Bantuan/SidebarHelp.jsx
+++ src/components/Bantuan/SidebarHelp.jsx
@@ -1,19 +1,23 @@
 import React from 'react';
 import Accordian from './Accordian';
+import Card from '@material-ui/core/Card';
 function SidebarHelp() {
     return (
-        <div style={{ margin: '50px' }}>
-            <h1 style={{ padding: '40px 0', fontSize: '46px' }}>
+        <div style={{ margin: '50px', color: 'black' }}>
+            <h1 style={{ padding: '30px 0', fontSize: '36px' }}>
                 Apa Yang Dapat Kami Bantu?
             </h1>
             <div style={{ marginBottom: '50px' }}>
-                <p> Jawaban untuk FAQ.</p>{' '}
+                <p>Jawaban untuk FAQ.</p>{' '}
                 <p>
                     Jika Anda membutuhkan bantuan, periksalah jika pertanyaan
                     Anda telah di jawab.
                 </p>
             </div>
-            <Accordian />
+
+            <Card>
+                <Accordian />
+            </Card>
         </div>
     );
 }
