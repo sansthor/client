52d4c62f2a5c9720973c8cc7b46b904e4b01d83a
renvelvet <reshapuspita734@gmail.com>
2020-08-09 14:37:20 +0700

    remove comment

--- src/pages/EditProfile/EditProfile.jsx
+++ src/pages/EditProfile/EditProfile.jsx
@@ -1,6 +1,6 @@
 import jwt_decode from 'jwt-decode';
 import React, { useEffect, useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
 import { useHistory } from 'react-router-dom';
 import { fetchPutBasicUser, fetchGetMember } from '../../redux/actions';
 import { Link } from 'react-router-dom';
